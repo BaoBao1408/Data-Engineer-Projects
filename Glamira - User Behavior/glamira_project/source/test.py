9bb785f632286702e6a07e6102f57e6c667adfbd
BaoBao1408 <117745745+BaoBao1408@users.noreply.github.com>
2026-03-18 16:30:36 +0700

    big update crawl sku

---
+++ Glamira - User Behavior/glamira_project/source/test.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017")
+db = client["glamira_raw"]
+
+product = db.products.find_one({"product_id": "110474"})
+
+data = product["react_data"]
+
+print({
+    "product_id": data["product_id"],
+    "name": data["name"],
+    "price": data["price"],
+    "category": data["category_name"],
+    "gender": data["gender"],
+    "collection": data["collection"]
+})
