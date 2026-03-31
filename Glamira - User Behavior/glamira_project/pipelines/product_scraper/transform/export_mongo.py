90a809835e80245f3b16d2427bd832deb6b8df52
BaoBao1408 <baoquocnguyen1408@gmail.com>
2026-03-31 09:19:27 +0700

    local update before pull

--- Glamira - User Behavior/glamira_project/pipelines/product_scraper/transform/export_mongo.py
+++ Glamira - User Behavior/glamira_project/pipelines/product_scraper/transform/export_mongo.py
@@ -20,27 +20,6 @@ def json_safe(doc):
 
 
 # =========================
-# FLATTEN PRODUCT
-# =========================
-
-# def flatten_product(doc):
-
-#     react = doc.get("react_data", {})
-
-#     return {
-#         "product_id": doc.get("product_id"),
-#         "url": doc.get("url"),
-#         "name": react.get("name"),
-#         "price": react.get("price"),
-#         "category": react.get("category_name"),
-#         "collection": react.get("collection"),
-#         "gender": react.get("gender"),
-#         "min_price": react.get("min_price"),
-#         "max_price": react.get("max_price")
-#     }
-
-
-# =========================
 # GENERIC EXPORT (STREAM + GZIP)
 # =========================
 
@@ -58,7 +37,7 @@ def export_stream(collection_name, output_file, transform=None):
 
     with gzip.open(output_file, "wt", encoding="utf-8") as f:
 
-        cursor = collection.find({}, {"_id": 0}).batch_size(1000)
+        cursor = collection.find({}).batch_size(1000)
 
         total = collection.estimated_document_count()
 
